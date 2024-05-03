716db85a6998c4257616a67f08dd8a802c03ba09
obadaajenah <95519889+obadaajenah@users.noreply.github.com>
2024-05-03 13:50:10 +0300

    My message

--- stopword.py
+++ stopword.py
@@ -9,6 +9,10 @@ text="""Hellwo My -- ///  name obadad and this my friend Abdo. we also live here
 stop_words = set(stopwords.words('english'))
 #print(stop_words)
 
+#def remove_stop_words(words):
+ #   stop_words = stopwords.words('english')
+  #  return [w for w in words if w not in stop_words]
+
 tokenize_words = word_tokenize(text)
 tokenize_words_without_stop_words = []
 for word in tokenize_words:
@@ -19,3 +23,4 @@ print(set(tokenize_words)-set(tokenize_words_without_stop_words))
 print(tokenize_words_without_stop_words)
 
 ###########ola and bayan ###########
+
